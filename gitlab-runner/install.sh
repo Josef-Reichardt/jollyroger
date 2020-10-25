66c8ada4b43370f14d9910a8cac6eb62e0688bda
Josef Reichardt <reichardt.josef@gmail.com>
2020-10-25 17:31:03 +0100

    Add nachhaltig-verstoert-website.yaml

--- gitlab-runner/install.sh
+++ gitlab-runner/install.sh
@@ -1,3 +1,4 @@
 microk8s.helm repo add gitlab https://charts.gitlab.io
 microk8s.helm repo update
-microk8s.helm install gitlab/gitlab-runner --namespace gitlab-runner --name gitlab-runner -f values.yaml
+microk8s.helm install gitlab/gitlab-runner --namespace gitlab-runner --name nachhaltig-verstoert-website-gitlab-runner -f nachhaltig-verstoert-website.yaml
+microk8s.helm install gitlab/gitlab-runner --namespace gitlab-runner --name nachhaltig-verstoert-content-gitlab-runner -f nachhaltig-verstoert-content.yaml
