d750285fee76bc3e6202bf03972c0b23f73defdd
Josef Reichardt <reichardt.josef@gmail.com>
2020-10-25 17:18:59 +0100

    Fix init script

--- gitlab-runner/init.sh
+++ gitlab-runner/init.sh
@@ -1,9 +1,9 @@
 
 if microk8s.helm list --namespace gitlab-runner | grep -q gitlab-runner
 then
-  helm repo add gitlab https://charts.gitlab.io
-  helm init
-  helm install --namespace gitlab-runner -name gitlab-runner -f values.yaml gitlab/gitlab-runner
+  microk8s.helm repo add gitlab https://charts.gitlab.io
+  microk8s.helm init
+  microk8s.helm install --namespace gitlab-runner -name gitlab-runner -f values.yaml gitlab/gitlab-runner
 else
-  helm upgrade --namespace gitlab-runner -f values.yaml gitlab-runner gitlab/gitlab-runner
+  microk8s.helm upgrade --namespace gitlab-runner -f values.yaml gitlab-runner gitlab/gitlab-runner
 fi;
