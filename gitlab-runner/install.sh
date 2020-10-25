f045c13eb2611f5f604cac989c061b598289d6e8
Josef Reichardt <reichardt.josef@gmail.com>
2020-10-25 17:26:52 +0100

    Update install.sh

--- gitlab-runner/install.sh
+++ gitlab-runner/install.sh
@@ -1,4 +1,3 @@
 microk8s.helm repo add gitlab https://charts.gitlab.io
 microk8s.helm repo update
-microk8s.helm init
-microk8s.helm install --namespace gitlab-runner -name gitlab-runner -f values.yaml gitlab/gitlab-runner
+microk8s.helm install gitlab/gitlab-runner --namespace gitlab-runner --name gitlab-runner -f values.yaml
