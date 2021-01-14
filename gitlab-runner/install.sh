b7370c6850c3f9b79f384593742f93abefa7c6a9
Josef Reichardt <reichardt.josef@gmail.com>
2021-01-14 21:37:34 +0100

    Update gitlab runner

--- gitlab-runner/install.sh
+++ gitlab-runner/install.sh
@@ -1,4 +1,3 @@
 microk8s.helm repo add gitlab https://charts.gitlab.io
 microk8s.helm repo update
-microk8s.helm install gitlab/gitlab-runner --namespace gitlab-runner --name nachhaltig-verstoert-website-gitlab-runner -f nachhaltig-verstoert-website.yaml
 microk8s.helm install gitlab/gitlab-runner --namespace gitlab-runner --name nachhaltig-verstoert-content-gitlab-runner -f nachhaltig-verstoert-content.yaml
