1fe035f85c17f02efd5cee591ce0a6e39a4fab65
Josef Reichardt <reichardt.josef+gitlab@gmail.com>
2024-12-15 15:33:28 +0000

    cleanup

--- gitlab-runner/install.sh
+++ gitlab-runner/install.sh
@@ -1,7 +1,7 @@
 microk8s.helm repo remove gitlab
 microk8s.helm repo add gitlab https://charts.gitlab.io
 microk8s.helm repo update
-microk8s.helm del nachhaltig-verstoert-content-gitlab-runner
-microk8s.helm install nachhaltig-verstoert-content-gitlab-runner gitlab/gitlab-runner --namespace gitlab-runner \
+microk8s.helm --namespace gitlab-runner del nachhaltig-verstoert-content-gitlab-runner
+microk8s.helm --namespace gitlab-runner install nachhaltig-verstoert-content-gitlab-runner gitlab/gitlab-runner \
   -f nachhaltig-verstoert-content.yaml \
   -f token.yaml
