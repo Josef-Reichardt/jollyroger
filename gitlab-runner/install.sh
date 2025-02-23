598a6eb979c830fc25c02f83e7b3eb710be447d2
Josef Reichardt <reichardt.josef+gitlab@gmail.com>
2025-02-23 16:32:41 +0100

    feat: add creativity-green-group-gitlab-runner

--- gitlab-runner/install.sh
+++ gitlab-runner/install.sh
@@ -1,7 +1,13 @@
 microk8s.helm repo remove gitlab
 microk8s.helm repo add gitlab https://charts.gitlab.io
 microk8s.helm repo update
+
 microk8s.helm --namespace gitlab-runner del nachhaltig-verstoert-content-gitlab-runner
 microk8s.helm --namespace gitlab-runner install nachhaltig-verstoert-content-gitlab-runner gitlab/gitlab-runner \
   -f nachhaltig-verstoert-content.yaml \
-  -f token.yaml
+  -f token-nachhaltig-verstoert.yaml
+
+microk8s.helm --namespace gitlab-runner del creativity-green-group-gitlab-runner
+microk8s.helm --namespace gitlab-runner install creativity-green-group-gitlab-runner gitlab/gitlab-runner \
+  -f creativity-green-group.yaml \
+  -f token-creativity-green.yaml
